010304a56848948d7b56373ae15ce2f7ed1f8fd9
lolinkgaming24-hash <lolinkgaming24@gmail.com>
2026-04-28 12:12:34 +0800

    Update that.js

--- that.js
+++ that.js
@@ -1,7 +1,7 @@
-const canvas = querySelector("canvas)
-const c = getContext("2d")
+const canvas = querySelector("canvas);
+const c = getContext("2d");
 
 canvas.width = 1500
 canvas.height = 590
 
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
